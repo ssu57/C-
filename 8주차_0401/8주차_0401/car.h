a58d5307318a7618e42cb31b5e72686f65c3176d
ssu57 <132045002+ssu57@users.noreply.github.com>
2024-04-01 10:54:57 +0900

    Add files via upload

---
+++ 8주차_0401/8주차_0401/car.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car
+{
+	int speed;//속도
+	int gear;//기어
+	string color;//색상
+public:
+	int getSpeed();
+	void setSpeed(int s);
+};
