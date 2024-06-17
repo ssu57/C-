2364bb0288d2543704771cb4e935b207106e15ca
ssu57 <132045002+ssu57@users.noreply.github.com>
2024-06-17 16:55:40 +0900

    Add files via upload

---
+++ 18주차_0527/18주차_0527/0527.c
@@ -0,0 +1,18 @@
+//구조체
+struct student {
+	int number;
+	char name[10];
+	double grade;
+};
+int main(void)
+{
+	struct student s;
+	s.number = 202325008;
+	strcpy(s.name, "원수민");
+	s.grade = 4.3;
+
+	printf("학번: %d\n", s.number);
+	printf("이름: %s\n", s.name);
+	printf("학점: %.2f\n", s.grade);
+	return 0;
+}
